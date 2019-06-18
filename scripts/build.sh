e846356cdccf5656fa82ef85a138ca2844e8d02b
Christian Berendt <berendt@betacloud-solutions.de>
2019-06-18 11:50:19 +0200

    Build an image only if it does not already exist

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -x
 
 # Available environment variables
 #
@@ -12,9 +11,20 @@ set -x
 BUILD_OPTS=${BUILD_OPTS:-}
 HASH_REPOSITORY=$(git rev-parse --short HEAD)
 
-docker build \
-    --build-arg "VERSION=$VERSION" \
-    --label "io.osism.${REPOSITORY#osism/}=$HASH_REPOSITORY" \
-    --tag "$REPOSITORY:$VERSION-centos-7-x86_64" \
-    --squash \
-    $BUILD_OPTS .
+# source: https://stackoverflow.com/questions/32113330/check-if-imagetag-combination-already-exists-on-docker-hub
+function docker_tag_exists() {
+    TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${TRAVIS_DOCKER_USERNAME}'", "password": "'${TRAVIS_DOCKER_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+    EXISTS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/$1/tags/?page_size=10000 | jq -r "[.results | .[] | .name == \"$2\"] | any")
+    test $EXISTS == true
+}
+
+if docker_tag_exists $REPOSITORY $VERSION-centos-7-x86_64; then
+    echo "The image $REPOSITORY:$VERSION already exists."
+else
+    docker build \
+        --build-arg "VERSION=$VERSION" \
+        --label "io.osism.${REPOSITORY#osism/}=$HASH_REPOSITORY" \
+        --tag "$REPOSITORY:$VERSION-centos-7-x86_64" \
+        --squash \
+        $BUILD_OPTS .
+fi
