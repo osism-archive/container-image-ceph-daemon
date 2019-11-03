84d94be447b9a5adbaa2997279a939cb50a56010
Christian Berendt <berendt@betacloud-solutions.de>
2019-11-03 14:09:23 +0100

    travis: overall improvements

--- scripts/push.sh
+++ scripts/push.sh
@@ -5,5 +5,7 @@
 # REPOSITORY
 # VERSION
 
-docker push "$REPOSITORY:$VERSION-centos-7-x86_64"
-docker rmi "$REPOSITORY:$VERSION-centos-7-x86_64"
+if [[ "$(docker images -q $REPOSITORY:$VERSION-centos-7-x86_64 2> /dev/null)" == "" ]]; then
+    docker push "$REPOSITORY:$VERSION-centos-7-x86_64"
+    docker rmi "$REPOSITORY:$VERSION-centos-7-x86_64"
+fi
--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,4 +6,6 @@ sudo apt -y install docker-ce
 echo '{ "experimental": true }' | sudo tee /etc/docker/daemon.json
 sudo service docker restart
 
-echo $TRAVIS_DOCKER_PASSWORD | docker login --username="$TRAVIS_DOCKER_USERNAME" --password-stdin
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    echo $TRAVIS_DOCKER_PASSWORD | docker login --username="$TRAVIS_DOCKER_USERNAME" --password-stdin
+fi
