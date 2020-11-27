c8a7ad1b8ad98c945c293c2fde60e898ba71a2c0
Christian Berendt <berendt@betacloud-solutions.de>
2020-11-27 18:08:51 +0100

    Add 'Check shell syntax' workflow & fix build workflow
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,24 +2,28 @@
 
 # Available environment variables
 #
+# DOCKER_REGISTRY
 # REPOSITORY
 # VERSION
 
 # Set default values
 
+DOCKER_REGISTRY=${DOCKER_REGISTRY:-quay.io}
 REPOSITORY=${REPOSITORY:-osism/ceph-daemon}
 VERSION=${VERSION:-latest}
 
-if [[ $VERSION == *"octopus"* ]]; then
+REPOSITORY="${DOCKER_REGISTRY}/${REPOSITORY}"
+
+if [[ "${VERSION}" == *"octopus"* ]]; then
     DISTRIBUTION=centos-8
 else
     DISTRIBUTION=centos-7
 fi
 
-if [[ $VERSION == "latest" ]]; then
+if [[ "${VERSION}" == "latest" ]]; then
     tag="latest"
 else
-    tag="$VERSION-$DISTRIBUTION-x86_64"
+    tag="${VERSION}-${DISTRIBUTION}-x86_64"
 fi
 
-docker push "$REPOSITORY:$tag"
+docker push "${REPOSITORY}:${tag}"
